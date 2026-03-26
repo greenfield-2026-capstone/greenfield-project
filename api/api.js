61b8b1737cab4dd6e57b6e5e7644529e758c5ea5
ruabee <annyrhdtjd@naver.com>
2026-03-26 16:46:44 +0900

    BE calculator

---
+++ api/api.js
@@ -0,0 +1,16 @@
+export default function handler(req, res) {
+  const { a, b } = req.query;
+
+  const numA = Number(a);
+  const numB = Number(b);
+
+  if (Number.isNaN(numA) || Number.isNaN(numB)) {
+    return res.status(400).json({ error: "a와 b는 숫자여야 합니다." });
+  }
+
+  return res.status(200).json({
+    a: numA,
+    b: numB,
+    result: numA + numB,
+  });
+}
